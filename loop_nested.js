bbc578cfb5f73790e5fbd0fa66494502987a148f
Sown13 <sonnh.buh@gmail.com>
2023-03-01 14:31:35 +0700

    Practice 3 nested loop

---
+++ loop_nested.js
@@ -0,0 +1,23 @@
+let table100, i, j;
+table100 = "<table>"
+for (i=1;i<=10;i++){
+    table100 += "<tr>";
+    for (j=1; j<=10;j++){
+        table100 = table100 + "<td>" + i*j + "</td>";
+    }
+    table100 += "<tr>";
+} table100 += "</table>";
+    document.write(table100);
+
+// i = j = 1;
+// while (i<=10){
+//     table100 += "<tr>";
+//     while (j<=10){
+//         table100 = table100 + "<td>" + i*j + "</td>";j++;
+//     }
+//     table100 += "</tr>";
+//     j=1;
+//     i++;
+// }
+// table100 += "<table>";
+// document.write(table100)
