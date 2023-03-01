ba948c34e54da013b17a5b9342ae4570a26a504d
Sown13 <sonnh.buh@gmail.com>
2023-03-01 13:44:43 +0700

    Practice 2 do while

--- loop_practice_2_while_dowhile.js
+++ loop_practice_2_while_dowhile.js
@@ -1,13 +1,13 @@
 function totalCal() {
     let num = parseInt(prompt("nhập vào '0' để dừng lại"));
     let total = 0
-    while (num !== 0) {
+    while (num !== -1) {
         num = parseInt(prompt("nhập vào '0' để dừng lại"));
-        console.log(num);
+        // console.log(num);
         total += num;
         console.log(num);
         alert(num);
-        console.log(total)
+        // console.log(total)
     }
     alert(total);
 }
@@ -25,3 +25,9 @@ while (i<=100){
     document.write("<hr style=width:" +i + "%>");
     i++;
 }
+
+let value = null;
+do {value = prompt("enter a number from 1->10");
+} while (value <1 || value >10);
+alert("bạn vừa nhập số" + value);
+
